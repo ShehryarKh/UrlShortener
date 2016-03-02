ddd1575c8199eaa7b9d54e735fab0d8a7ddbc6e4
Shehryar <shehryar2212@yahoo.com>
2016-03-02 12:44:03 -0500

    ignored env

--- UrlShortener/models.py
+++ UrlShortener/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import URLValidator
+
 
 # Create your models here.
+
+
+class UrlField(models.Model):
+	field = models.TextField(Validators=[URLValidator()])
